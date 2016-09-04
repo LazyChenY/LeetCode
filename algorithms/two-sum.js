619b3fe1791a67e65c89e453e54a84586897fc2e
Lazy <310195875@qq.com>
2016-09-04 11:19:40 +0800

    solution of two-sum
    use ‘map’ to replace the nested loop; reduce the run-time complexity;

--- algorithms/two-sum.js
+++ algorithms/two-sum.js
@@ -1,28 +1,21 @@
 /**
- * Created by apple on 16/9/3.
+ * Created by apple on 16/9/4.
  */
-
-console.time('time of twoSun');
-var twoSum = function(numbers, target) {
-	var map = {},
-		length = numbers.length,
+var twoSum = function(nums, target) {
+	var map={},
 		value,
-		anotherValue,
-		result = [];
-
-	for(var i = 0; i < length; i++) {
-		value = numbers[i];
-		anotherValue = target - value;
-		if (anotherValue in map) {
-			result.push(map[anotherValue]);
+		neededValue,
+		result=[];
+	for(var i=0;i<nums.length;i++){
+		value=nums[i];
+		neededValue=target-value;
+		if(value in map){
+			result.push(map[value]);
 			result.push(i);
-			break;
-		} else {
-			map[value] = i;
+		}else{
+			map[neededValue]=i;
 		}
 	}
 	return result;
 };
-console.timeEnd('time of twoSun');
-console.log(twoSum([2, 7, 9, 11,4,6,5], 11));
-console.log();
+
