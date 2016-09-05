8fae611f215f21563a0f8a189141a46e26e7c4a6
Lazy <310195875@qq.com>
2016-09-05 23:25:28 +0800

    add two number
    LinkList

---
+++ algorithms/add_two_number.js
@@ -0,0 +1,65 @@
+/**
+ * Created by _Lazy_ on 2016/9/5.
+ */
+
+function ListNode(val) {
+     this.val = val;
+     this.next = null;
+ }
+
+var addTwoNumbers = function(l1, l2) {
+	var val = 0,
+		newHead,
+		newTail,
+		node;
+
+	if (!l1) {
+		return l2;
+	}
+
+	if (!l2) {
+		return l1;
+	}
+
+	while (l1 && l2) {
+		val += l1.val + l2.val;
+		node = new ListNode(val % 10);
+
+		if (newHead) {
+			newTail.next = node;
+			newTail = newTail.next;
+		} else {
+			newHead = node;
+			newTail = node;
+		}
+
+		val = (val >= 10)? 1 : 0;
+		l1 = l1.next;
+		l2 = l2.next;
+	}
+
+	while (l1) {
+		val += l1.val;
+		node = new ListNode(val % 10);
+		newTail.next = node;
+		newTail = newTail.next;
+		val = (val >= 10)? 1 : 0;
+		l1 = l1.next;
+	}
+
+	while (l2) {
+		val += l2.val;
+		node = new ListNode(val % 10);
+		newTail.next = node;
+		newTail = newTail.next;
+		val = (val >= 10)? 1 : 0;
+		l2 = l2.next;
+	}
+
+	if (val > 0) {
+		node = new ListNode(val);
+		newTail.next = node;
+	}
+
+	return newHead;
+};
