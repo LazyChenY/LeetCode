b1997872fdc27b8a4babe2993db2b062a8e64b9b
lazy <310195875@qq.com>
2016-09-16 13:12:36 +0800

    change the palindrome

--- algorithms/longest_palindromic_substring.js
+++ algorithms/longest_palindromic_substring.js
@@ -1,44 +1,29 @@
 /**
  * Created by _Lazy_ on 2016/9/11.
- * ......It's messed up in my brain,
+ * dynamic record position
  */
-
-function makeOdd(str){
-	var len = str.length;
-	if(len % 2 === 1){
-		return str;
+var longestPalindrome = function (s) {
+	var start = 0,
+		end = 0,
+		max = 1,
+		position = [],
+		len = s.length;
+	for (var i = 0; i < len; i++) {
+		position.push(new Array(len));
+		position[i][i] = true;
 	}
-	var newStr = '#';
-	for(i = 0;i<len;i++){
-		newStr += str[i]+'#';
-	}
-	return newStr;
-}
-function judge(str){
-	(str.length%2 === 0) && (str = makeOdd(str));
-	var len = str.length,
-		half = Math.floor(len/2),
-		last = len-half;
-	var i = 0;
-	while(i<=last){
-		if(str[half-i] !== str[half+i]){
-			return false;
-		}
-		i++;
-	}
-	return true;
-}
-function getAllSubs(str){
-	var len = str.length,
-		res = [];
-	for(var i = 0;i<len;i++){
-		for(var j = i;j<len;j++){
-			var sub = str.substring(i,j+1);
-			// console.error(sub);
-			if(sub && judge(sub)){
-				res[res.length] = sub;
+	for (i = len - 1; i >= 0; i--) {
+		for (var j = i + 1; j < len; j++) {
+			if (s.charAt(i) == s.charAt(j) && (j == i + 1 || position[i + 1][j - 1])) {
+				position[i][j] = true;
+				if (j - i + 1 > max) {
+					max = j - i + 1;
+					start = i;
+					end = j;
+				}
 			}
 		}
 	}
-	return res;
-}
+	return s.slice(start, end + 1);
+};
+
